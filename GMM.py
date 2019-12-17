0e3fbe657d0f52a9bf596e9477c60b0ddb4a8929
hucaigang <51323415+hucaigang@users.noreply.github.com>
2019-12-17 17:08:13 +0800

    Update GMM.py

--- GMM.py
+++ GMM.py
@@ -8,7 +8,7 @@ Created on Thu Dec 12 17:45:18 2019
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
-
+from matplotlib.patches import Ellipse#做圆
 def dataset_select(dataset_name):
     if(dataset_name=='OFD'):
         return pd.read_csv('H://机器学习大作业//GMM(gaussian mixture model)andEM//datasets//faithful.csv')
@@ -22,14 +22,6 @@ def dim_trans(point):#将切片后的numpy数组维度确定，防止出错
     for i in range(point.shape[0]):
         temp_point[i]=point[i]
     return temp_point
-
-def ellipse(x,y,hor_len,vet_len,rotate_angle,color='c',count=10000): #画椭圆
-    points=np.zeros((count,2))
-    for i in range(count):
-        j=float(i)/count * 2 * np.pi
-        points[i,0]=x+hor_len*np.cos(j)*np.cos(rotate_angle)-vet_len*np.sin(j)*np.sin(rotate_angle)
-        points[i,1]=y+vet_len*np.sin(j)*np.cos(rotate_angle)+hor_len*np.cos(j)*np.sin(rotate_angle)
-    plt.plot(points[:,0],points[:,1],c=color,linestyle='--')
     
 def Nor_dis(point,avg,cov,dim):
     temp_point=dim_trans(point)
@@ -44,7 +36,7 @@ def Nor_dis(point,avg,cov,dim):
 class GMM():#聚类中心的选择问题。
     def __init__(self,data_set,init_avg_mat,max_iter_times):
         #DATA_PART 
-        self.char_num=data_set.shape[1]-1 #记录多少个特征
+        self.char_num=len(data_set.columns)-1 #记录多少个特征
         self.pts_num=data_set.shape[0] #记录多少数据点
         self.char_name=data_set.columns #特征名
         self.char=[] #保存特征数据值 1x272
@@ -59,7 +51,7 @@ class GMM():#聚类中心的选择问题。
         self.center_points=[]#初始化
         for i in range(self.K):
             self.center_points.append(self.avg_mat[i])
-        self.cov_mat=[np.diag(np.array([5,5]))]*self.K ##covariance matrix datatype:list[np array]
+        self.cov_mat=[np.diag(np.array([2,2]))]*self.K ##covariance matrix datatype:list[np array]
         #postier probability matrix
         self.P=np.zeros((self.K,self.pts_num),dtype=np.float64) #临时存储，放置每个点在每个模型下的值,size(K , n)
         
@@ -92,27 +84,52 @@ class GMM():#聚类中心的选择问题。
             self.M_step()
             
     def draw_pic(self):
-        color_list=['b','g','k','y','c','orange'] #中心点颜色选择
+        fig=plt.figure()
+        ax = plt.subplot()
+        color_list=['b','g','k','black','c','r'] #中心点颜色选择
         marker_list=['1','.','^','v','2','3'] #中心点图标选择
         if(self.char_num<=2):
-            plt.scatter(self.char[0],self.char[1],s=20,c='r') #x eruption y waiting time
+            plt.scatter(self.char[0],self.char[1],s=20,c='y') #x eruption y waiting time
             plt.xlabel(self.char_name[1])
             plt.ylabel(self.char_name[2])
             for i in range(self.iter_times+1):#迭代第几次
                 for j in range(self.K): #第k个模型的中心点
                     plt.scatter(self.center_points[self.K*i+j][0,0],self.center_points[self.K*i+j][1,0],
-                                s=200,c=color_list[j],marker=marker_list[j])
-            plt.legend(['point1','point2'])
-            #axis_len=np.linalg.eig(self.cov_mat) #0:value 1:vector
-            #rotate_angle=np.arccos(np.array([0,1]).dot(axis_len[1][0][:,1]))
-            #ellipse(self.avg_mat[0][0,0],self.avg_mat[0][1,0],axis_len[0][0,0],axis_len[0][0,1],rotate_angle)
-            #ellipse(self.avg_mat[1][0,0],self.avg_mat[1][1,0],axis_len[0][1,0],axis_len[0][1,1],rotate_angle)
-            plt.show()
-            
+                                s=200,c=color_list[j],marker=marker_list[j]) #散点图
+            fig.legend([i+1 for i in range(self.K)])
+            for i in range(self.K):#开始画置信椭圆
+                vals,vecs=np.linalg.eigh(self.cov_mat[i])
+                x,y=vecs[:,1]
+                theta = np.degrees(np.arctan2(y,x))
+                h,w=np.sqrt(5.991*vals)  #0.95置信度(可以修改)
+                ell=Ellipse(self.avg_mat[i],2*w,2*h,theta,color=color_list[5-i],linewidth=2,linestyle='--')
+                ell.set_alpha(0.5)
+                ell.set_fill(False)
+                ax.add_artist(ell)
+                plt.show()
+            fig.show()
+        
 if __name__=="__main__":
-    dataset_name=str(input("input the name of the dataset(OFD or Iris)"))
-    dataset=dataset_select(dataset_name) 
-    init_mu_mat=[np.array([[3],[20]]),np.array([[1],[90]])] #初始均值矩阵 (手动给定)
-    gmm1=GMM(dataset,init_mu_mat,30) #修改参数 (数据集，初始均值矩阵，迭代次数)
-    gmm1.run()
-    gmm1.draw_pic()
+    dataset_name=str(input('OFD or Iris or nothing'))
+    dataset=dataset_select(dataset_name)
+    if dataset_name=='OFD':
+        #对于Old Faithful dataset(right)
+        init_mu_mat=[np.array([[1],[30]]),np.array([[4],[90]])]
+        gmm1=GMM(dataset,init_mu_mat,30)
+        gmm1.run()
+        gmm1.draw_pic()
+    elif dataset_name=='Iris':
+    #对于Iris
+        index=['Sepal.Length','Sepal.Width','Petal.Length','Petal.Width','Species']#因为只能做两个特征的可视化，所以需要选择去除哪些特征
+        dataset.drop([index[0],index[1],index[4]],axis=1,inplace=True)#删除第2、4、5个特征
+        init_mu_mat=[np.array([[4],[2]]),np.array([[3],[1.1]]),np.array([[2],[1.2]])] #初始均值矩阵 (手动给定)对于Iris
+        gmm1=GMM(dataset,init_mu_mat,30) #修改参数 (数据集，初始均值矩阵，迭代次数)
+        gmm1.run()
+        gmm1.draw_pic()
+    else:
+        #对于自建数据集
+        init_mu_mat=[np.array([[3],[3]]),np.array([[20],[20]]),np.array([[90],[90]])]
+        gmm1=GMM(dataset,init_mu_mat,50)
+        gmm1.run()
+        gmm1.draw_pic()
+    
